7adbfa51ed7f829b51bbc508e68af668d8c3f717
tswmHer <tyther0@gmail.com>
2022-07-14 13:24:31 -0500

    reset button

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ $(document).ready ( () => {
     let counter = 1;
     let spaces = Array(9).fill(null);
     let currentPlayer = 'X';
+
+    // Game winning Combinations
     const winningCombos = [
         [1,2,3],
         [1,4,7],
@@ -15,16 +17,16 @@ $(document).ready ( () => {
     ];
 
     $('table tr td').click(function (e) {
-        let id = e.target.id;
+        let id = e.target.id; // targert id of the box selected.
         
 
         if ( $(this).text() == '' && turn) {
             if ( turn === 1 ) {
                 $(this).append('X');
                 $(this).css('color', 'red');
-                currentPlayer = $(this).text();
+                currentPlayer = $(this).text(); // current player becomes the appended string
                 spaces[id] = currentPlayer;
-                e.target.innerText = currentPlayer;
+                e.target.innerText = currentPlayer; // selected box becomes current player
                 counter++;
                 turn = 2;
                 
@@ -44,11 +46,21 @@ $(document).ready ( () => {
         checkWin();
 
         if ( checkWin() != false ) {
-            alert (`${currentPlayer} is the Winner!\n\nEnd of Game`);
-            reset();
+            $('#tic-tac-toe').text(`${currentPlayer} is the Winner! End of Game`); // change the title to the winner
+            $('#myAlert').append('<button class="btn-styled" type="button"> RESET </button>'); // create a button that appears after a game is done
+            $('button').click(function () {
+                reset ()
+            });
+            // alert (`${currentPlayer} is the Winner!\n\nEnd of Game`);
+            // reset();
         } else if( counter > 9) {
-            alert ('TIE');
-            reset();
+            // alert ('TIE');
+            $('#tic-tac-toe').text(`TIE! End of Game`);
+            $('#myAlert').append('<button class="btn-styled" type="button"> RESET </button>');
+            $('button').click(function () {
+                reset ()
+            });
+            // reset();
         } 
        
     });
@@ -56,7 +68,8 @@ $(document).ready ( () => {
     function checkWin() {
         for (let condition of winningCombos) {
             let [a,b,c] = condition;
-            if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            // need to compare winning combos with each players
+            if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) { 
                 return [a,b,c]
             } 
         }
@@ -65,11 +78,13 @@ $(document).ready ( () => {
 
     function reset () {
         $('table tr td').empty();
-        currentPlayer = 'X'
-        turn = 1;
-        counter = 1
-        spaces = Array(9).fill(null);
-    
+        $('#tic-tac-toe').text('TIC TAC TOE')
+        $('button').remove();
+            currentPlayer = 'X'
+            turn = 1;
+            counter = 1
+            spaces = Array(9).fill(null);
+        
     };
   
 });
